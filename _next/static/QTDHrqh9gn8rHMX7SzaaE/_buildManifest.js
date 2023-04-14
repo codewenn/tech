aa0114cb787a54d0c6b67939dd34c91cc35778ba
J-Wenn <53149679+J-Wenn@users.noreply.github.com>
2023-04-14 14:58:25 +0000

    Deploying to gh-pages from @ codewenn/tech@ee626a0e6852f9503b8c413d4f1109a6666bebe0 🚀

---
+++ _next/static/QTDHrqh9gn8rHMX7SzaaE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-5daf0e81640816c1.js"],"/_error":["static/chunks/pages/_error-0dfe53c66a164c7e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
